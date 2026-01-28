7bd355a46c0c95c8adecbd036b64e91d0d287791
Amy Huang <amyxjhuang@berkeley.edu>
2026-01-27 23:43:47 -0500

    Add starter files

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10;
+    private Rigidbody rb;
+    private float movementX;
+    private float movementY;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x;
+        movementY = movementVector.y;
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(new Vector3(movementX, 0.0f, movementY) * speed);
+    }
+}
